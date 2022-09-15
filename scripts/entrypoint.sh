0845ee1fc3f05a2418a21fac8d2fcc246e53f332
Ian Charters <icharters@coalitioninc.com>
2022-09-15 08:45:29 -0700

    wip

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,2 @@
+# https://hub.docker.com/layers/library/kong/latest/images/sha256-8a54bbf5026200150c46a182e0d56d74ba7572d6cfe98e863fb20cdafce1d95e?context=explore
+# Kong docker container's start command: `CMD ["kong" "docker-start"]`
