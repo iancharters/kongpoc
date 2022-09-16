89f7d0e3d430bfc1171ffceec8058e164e04220e
Ian Charters <icharters@coalitioninc.com>
2022-09-15 22:24:56 -0700

    wip

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-# if not a 200 or invalid PEM we should abort
-# after we set we should assert the value
-
 KONG_CONFIG=../config/kong.yaml
 PUB_KEY=pubkey.pem
 
 # Retrieve X509 certificate
 curl -o $PUB_KEY $AUTH0_ISSUER_URL/pem
 
-# Extract public key from cert
+# Extract public key from cert.  This will fail with a non-zero exit code if the
+# previous step did not produce a proper certificate for whatever reason.
 openssl x509 -pubkey -noout -in $PUB_KEY > .tmp && mv .tmp $PUB_KEY
 
 # Inject public key into auth0 consumer rsa_public_key field
